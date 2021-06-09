8060a0a014aa1b386d1799da27ad1d1fe69f1469
Quan <manhquan24072000@gmail.com>
2021-06-09 16:03:50 +0700

    Update Code

--- index.php
+++ index.php
@@ -43,12 +43,14 @@
 	      		while ($result_new = $product_new->fetch_assoc()) {
 	      			      	
 	      	 ?>
+				<div class="row">
 				<div class="col-sm-4 images_1_of_4">
 					 <a href="details.php"><img src="admin/uploads/<?php echo $result_new['image'] ?>" alt="" /></a>
 					 <h2><?php echo $result_new['productName'] ?></h2>
 					 <p><?php echo $fm->textShorten($result_new['product_desc'], 50) ?></p>
 					 <p><span class="price"><?php echo $fm->format_currency($result_new['price'])." VND" ?></span></p>
 				     <div class="button"><span><a href="details.php?proid=<?php echo $result_new['productId'] ?>" class="details">Chi tiết</a></span></div>
+				</div>
 				</div>
 			<?php 
 				}
